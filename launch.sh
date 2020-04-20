a59536297e4fa6cede337379956557819b8e958b
William Belle <william.belle@gmail.com>
2020-04-20 18:59:12 +0200

    Update launch script

--- launch.sh
+++ launch.sh
@@ -6,7 +6,13 @@ sleep 2
 
 DIR=`dirname "$0"`
 
-conky -d -c ${DIR}/conky-ubuntu
-conky -d -c ${DIR}/conky-gnome
 conky -d -c ${DIR}/conky-info
 conky -d -c ${DIR}/conky-cpu
+conky -d -c ${DIR}/conky-memory
+
+conky -d -c ${DIR}/conky-ubuntu
+conky -d -c ${DIR}/conky-gnome
+
+conky -d -c ${DIR}/conky-network
+conky -d -c ${DIR}/conky-hdd
+conky -d -c ${DIR}/conky-gpu
